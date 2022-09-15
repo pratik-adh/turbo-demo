2c097eae5802893386f33fe44682595e768da6d3
pratik-adh <adhikaripratik014@gmail.com>
2022-09-15 15:59:28 +0545

    workflow of turborepo after adding a package

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,14 +1,32 @@
+import { Input } from "antd";
 import { useCallback, useMemo, useState } from "react";
 import { Button } from "ui";
 
-export default function Docs() {
-  const [showText, setShowText] = useState(false);
+export default function Web() {
+  const [showText, setShowText] = useState<Boolean>(false);
+  const [name, setName] = useState<string>("");
+  const [nameoOnClick, setNameOnClick] = useState<string>("");
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    console.log(e);
+    setName(e.target.value);
+  };
 
   return (
     <div>
       <h1>Docs</h1>
-      <Button title="Press" onPress={() => setShowText(!showText)} />
-      {showText && <h2>I am a testing turborepo on docs</h2>}
+      <h2>Turborepo test on Docs</h2>
+      <Input placeholder="Enter your name" onChange={onChange} />
+      <Button
+        title="Press"
+        onPress={() => {
+          setShowText(name !== "" && true);
+          setNameOnClick(name);
+        }}
+      />
+      {showText && <h2>Your name in docs is {nameoOnClick}.</h2>}
     </div>
   );
 }
